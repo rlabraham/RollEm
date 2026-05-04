1b33bd137d40106a7127da61b0c4a7ca548aacf6
Richard Abraham <richard.lee.abraham@gmail.com>
2026-05-04 09:43:09 -0500

    new build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.rltech.rollem"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
